cec8f9182d10eef22f2e0c8f02e3bbe691f49f36
xa3687va <xander.van.der.weken@fh-erfurt.de>
2023-08-09 22:47:22 +0200

    Added First Bits

---
+++ src/components/Editor.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="editor-container">
+        <div class="editor-content">
+
+        </div>
+        <div class="editor-preview">
+
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Makrobar.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="makrobar">
+        <button class="makro-btn">Makro 1</button>
+        <button class="makro-btn">Makro 2</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
