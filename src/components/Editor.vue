36d6416c4c87762bd05accdbae3e96e6a2171219
xa3687va <xander.van.der.weken@fh-erfurt.de>
2023-08-10 09:16:33 +0200

    Made Editor work and pretty

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,16 +1,21 @@
 <template>
     <div class="editor-container">
         <div class="editor-content">
-            <textarea cols="50" rows="30" autofocus></textarea>
+            <textarea cols="50" rows="30" placeholder="Start Typing here..." v-model="htmlInput" @input="updatePreview()" autofocus></textarea>
         </div>
-        <div class="editor-preview">
-            <textarea cols="50" rows="30" disabled></textarea>
-        </div>
+        <div class="editor-preview" v-html="renderedHtml"></div>
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 
+const htmlInput = ref("");
+const renderedHtml = ref("");
+
+function updatePreview() {
+    renderedHtml.value = htmlInput.value;
+}
 </script>
 
 <style scoped>
@@ -18,5 +23,20 @@
     display: flex;
     flex-direction: row;
     gap: 10px;
+}
+
+textarea {
+    flex: 1;
+    width: 100%;
+    height: 100%;
+    resize: none;
+    border: 1px solid #ccc;
+}
+
+.editor-preview {
+    padding: 10px;
+    flex: 1;
+    border: 1px solid #ccc;
+    overflow: auto;
 }
 </style>
