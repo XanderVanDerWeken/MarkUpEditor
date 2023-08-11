3f11c3253c5f2b63c575257215fbbda235a1db12
xa3687va <xander.van.der.weken@fh-erfurt.de>
2023-08-11 21:27:29 +0200

    Added Menubar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <header>
-    <h1>MarkUp Editor</h1>
-    <a href="/editor">ed</a>
+    <Menubar />
   </header>
   
   <main>
@@ -10,32 +9,9 @@
 </template>
 
 <script setup lang="ts">
+import Menubar from './components/Menubar.vue';
 
 </script>
 
 <style>
-.flex-center {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo.electron:hover {
-  filter: drop-shadow(0 0 2em #9FEAF9);
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ src/components/Menubar.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav>
+        <h3>MarkUp Editor</h3>
+        <button @click="navigate('/')">Home</button>
+    </nav>
+</template>
+
+<script setup lang="ts">
+import router from '../router/router';
+
+function navigate(path: string) {
+    router.push( path );
+}
+</script>
+
+<style scoped>
+nav {
+    display: inline;
+    background-color: #ddd;
+    padding: 10px;
+}
+
+h3 {
+    display: inline;
+}
+
+button {
+    margin: 5px;
+}
+</style>
