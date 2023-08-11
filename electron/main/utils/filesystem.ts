abc19cb7be8cf59bd9b71822996e8ce445c162d3
xa3687va <xander.van.der.weken@fh-erfurt.de>
2023-08-11 21:00:39 +0200

    Seperated FS

--- electron/main/utils/filesystem.ts
+++ electron/main/utils/filesystem.ts
@@ -1,4 +1,4 @@
-import { ipcMain, ipcRenderer } from "electron";
+import { ipcMain } from 'electron';
 import fs from 'fs/promises';
 
 class Filesystem {
@@ -30,33 +30,6 @@ class Filesystem {
                 return [];
             }
         })
-    }
-
-    async saveFile(data: string, filename: string): Promise<string | null> {
-        try {
-            return await ipcRenderer.invoke('save-file', data, filename);
-        } catch (error) {
-            console.error( error );
-            return null;
-        }
-    }
-
-    async loadFile(filename: string): Promise<string | null> {
-        try {
-            return await ipcRenderer.invoke('load-file', filename);
-        } catch (error) {
-            console.error( error );
-            return null;
-        }
-    }
-
-    async getFilenames(): Promise<string[]> {
-        try {
-            return await ipcRenderer.invoke('get-filenames');
-        } catch (error) {
-            console.error( error );
-            return [];
-        }
     }
 }
 
