063a9c77657f8cc0896274359f154aa77e18a801
xa3687va <xander.van.der.weken@fh-erfurt.de>
2023-08-15 11:17:04 +0200

    Added Makros to Config

--- src/models/config.ts
+++ src/models/config.ts
@@ -1,5 +1,15 @@
+import Makro from "./makro";
+
 class Config {
-    
+    private makros: Makro[]
+
+    constructor(makros: Makro[]) {
+        this.makros = makros;
+    }
+
+    public getMakros(): Makro[] {
+        return this.makros;
+    }
 }
 
 export default Config;
--- src/views/ConfigView.vue
+++ src/views/ConfigView.vue
@@ -1,9 +1,63 @@
 <template>
+    <h1>Einstellungen</h1>
+    <h2>Makros</h2>
+    <div class="makrosContainer">
+        <div class="makrosTable">
+            <table>
+                <thead>
+                    <td>Makro</td>
+                    <td>Details</td>
+                </thead>
+                <tr v-for="makro in config?.getMakros()">
+                    <td>{{ makro.getTag() }}</td>
+                    <td>{{ makro.getConfig() }}</td>
+                </tr>
+            </table>
+        </div>
+        <div class="newMakro">
+            <form>
+                <h3>Neuer Makro</h3>
+                <label for="newMakroTag">Makro Tag:</label><br>
+                <input 
+                    type="text"
+                    id="newMakroTag"
+                    v-model="newMakro.tag"
+                ><br>
+                <label for="newMakroConfig">Makro Details:</label><br>
+                <input
+                    type="text"
+                    id="newMakroConfig"
+                    v-model="newMakro.config"
+                ><br>
+                <button @click="addNewMakro()">hinzufügen</button>
+            </form>
+        </div>
+    </div>
 
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import Filesystem from '../utils/filesystem';
+import Config from '../models/config';
+import Makro from '../models/makro';
 
+const filesystem = new Filesystem();
+
+const config = ref<Config | null>();
+const newMakro = ref({ tag: '', config: '' });
+
+function addNewMakro() {
+    const makroToAdd = new Makro(newMakro.value.tag, newMakro.value.config);
+    config.value?.getMakros().push(makroToAdd);
+    if(config.value) {
+        filesystem.saveConfig( config.value );
+    }
+}
+
+onMounted( async () => {
+    config.value = await filesystem.loadConfig();
+})
 </script>
 
 <style scoped>
